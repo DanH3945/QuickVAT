40e6f49da6e454492235ca909080b30aa75aace2
Heretic <hereticpurge@gmail.com>
2019-03-10 08:05:36 -0600

    Basic fragment switching setup

--- app/src/main/java/com/hereticpurge/quickvat/MainActivity.java
+++ app/src/main/java/com/hereticpurge/quickvat/MainActivity.java
@@ -1,5 +1,8 @@
 package com.hereticpurge.quickvat;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void loadFragment(Fragment fragment, boolean addToBackStack, String tag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment_container, fragment);
+
+        if (addToBackStack) fragmentTransaction.addToBackStack(tag);
+        fragmentTransaction.commit();
+        fragmentManager.executePendingTransactions();
     }
 }
