a03ab887452fef6213dda4a47af3015b0472ba11
Heretic <hereticpurge@gmail.com>
2019-05-11 04:18:05 -0600

    Switched VatDisplayFragment to a list listener instead of single item to get ready for further buildout.
    Added Timber startup calls to Preference and Display Fragments.

--- app/src/main/java/com/hereticpurge/quickvat/PreferenceFragment.java
+++ app/src/main/java/com/hereticpurge/quickvat/PreferenceFragment.java
@@ -1,6 +1,14 @@
 package com.hereticpurge.quickvat;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import timber.log.Timber;
 
 public class PreferenceFragment extends Fragment {
 
@@ -8,5 +16,12 @@ public class PreferenceFragment extends Fragment {
 
     public static Fragment createInstance() {
         return new PreferenceFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Timber.d("Fragment Loading %s", TAG);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
--- app/src/main/java/com/hereticpurge/quickvat/VatDisplayFragment.java
+++ app/src/main/java/com/hereticpurge/quickvat/VatDisplayFragment.java
@@ -14,9 +14,16 @@ import android.widget.TextView;
 import com.hereticpurge.quickvat.appmodel.CountryObject;
 import com.hereticpurge.quickvat.appmodel.QuickVatViewModel;
 
-public class VatDisplayFragment extends Fragment implements Observer<CountryObject> {
+import java.util.List;
+
+import timber.log.Timber;
+
+public class VatDisplayFragment extends Fragment implements Observer<List<CountryObject>> {
 
     public static final String TAG = "VatDisplayFragment";
+
+    private List<CountryObject> mCountryList;
+    private CountryObject mSelectedCountry;
 
     QuickVatViewModel mViewModel;
 
@@ -29,19 +36,26 @@ public class VatDisplayFragment extends Fragment implements Observer<CountryObje
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Timber.d("Fragment Loading %s", TAG);
         View view = inflater.inflate(R.layout.vat_display_fragment_layout, container, false);
 
         mTextView = view.findViewById(R.id.vat_display_main_text);
 
         mViewModel = ViewModelProviders.of(this).get(QuickVatViewModel.class);
 
-        mViewModel.getSingleCountryObjectById(1).observe(this, this);
+        mViewModel.getCountryObjects().observe(this, this);
+        Timber.d("Added ViewModel Observer");
 
         return view;
     }
 
     @Override
-    public void onChanged(@Nullable CountryObject countryObject) {
-        mTextView.setText(countryObject.getCountryName());
+    public void onChanged(@Nullable List<CountryObject> countryObjects) {
+        Timber.d("Country List Updating");
+        mCountryList = countryObjects;
+    }
+
+    private void updateUI() {
+        Timber.d("Updating UI");
     }
 }
